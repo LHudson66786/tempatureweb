3a8e7f3ad257a84f99b3c681a10ef958a9dafc20
lukas <98899236+LHudson66786@users.noreply.github.com>
2024-02-26 15:40:42 -0500

    git commit

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask, jsonify
+from pprint import pprint
 import os
 
-app = Flask(__name__)
+from flask import Flask, render_template, request
+import requests
 
+app = Flask(__name__)
+api_key = 'eab14b134c19ebda9ad67fa887f2e4fe'
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return render_template('home.html')
+
+@app.route('/weather')
+def weather():
+    location = request.args.get('location')
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
+    response = requests.get(url).json()
+    pprint(response)
+    temperature = response['main']['temp']
+    return f'The temperature in {location} is {temperature} degrees Celsius.'
 
 
 if __name__ == '__main__':
